025eca7b0dbdc95fb06601de31c70e1f7a6a08aa
Inori <1121562648@qq.com>
2016-12-05 21:02:55 +0800

    yinjiankang-commit-005

--- src/main/java/com/cuit/boke/service/ArticleService.java
+++ src/main/java/com/cuit/boke/service/ArticleService.java
@@ -1,21 +1,10 @@
 package com.cuit.boke.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.cuit.boke.dao.ArticleDao;
 import com.cuit.boke.dto.PageBean;
 import com.cuit.boke.entity.Article;
 
-@Service
-@Transactional
-public class ArticleService {
-	
-	@Autowired
-	private ArticleDao articleDao;
+
+public interface ArticleService {
 	
 	/**
 	 * 分页查询文章
@@ -23,29 +12,13 @@ public class ArticleService {
 	 * @param pageSize 每页显示的条数
 	 * @return 分页实体
 	 */
-	public PageBean<Article> recentArticleByPage(int currPage, int pageSize){
-		PageBean<Article> pageBean = new PageBean<Article>();
-		//封装当前页数
-		pageBean.setCurrPage(currPage);
-		//封装每页显示的记录数
-		pageBean.setPageSize(pageSize);
-		//封装总记录条数
-		int totalCount = articleDao.queryTotalCount();
-		pageBean.setTotalCount(totalCount);
-		//封装总页数
-//		Double pageCount = Math.ceil(totalCount / pageSize);
-		int pageCount = (totalCount + pageSize -1) / pageSize;
-		pageBean.setTotalPage(pageCount);
-		//封装每页显示的数据
-		int begin = (currPage - 1)*pageSize;
-		System.out.println("---------Service");
-		List<Article> list = articleDao.queryByPage(begin, pageSize);
-		pageBean.setList(list);
-		return pageBean;
-	}
+	public PageBean<Article> recentArticleByPage(int currPage, int pageSize);
 	
-	public int saveArticle(Article article){
-		return articleDao.insert(article);
-	}
+	/**
+	 * 保存文章
+	 * @param article
+	 * @return 保存文章的id
+	 */
+	public int saveArticle(Article article);
 
 }
--- src/main/java/com/cuit/boke/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.cuit.boke.service;
-
-import com.cuit.boke.entity.User;
-
-public interface UserService {
-	
-	public int insertUser(User user);
-	
-}
